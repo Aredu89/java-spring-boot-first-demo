62890affad43e0f5ca3188b7a95efb1c81825197
Aredu89 <aredu_r89@hotmail.com>
2026-03-10 17:31:55 -0500

    Dependency injection

---
+++ src/main/java/com/springboot/demo/MySpringApp/entity/Coach.java
@@ -0,0 +1,6 @@
+package com.springboot.demo.MySpringApp.entity;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
--- src/main/java/com/springboot/demo/MySpringApp/rest/MyRestController.java
+++ src/main/java/com/springboot/demo/MySpringApp/rest/MyRestController.java
@@ -1,5 +1,7 @@
 package com.springboot.demo.MySpringApp.rest;
 
+import com.springboot.demo.MySpringApp.entity.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyRestController {
     @Value("${team.name}")
     private String teamName;
+    private Coach myCoach;
     // add code to expose "/" that return "Hello World"
 
     @GetMapping("/")
      public String sayHello() {
          return "Edited. Team Name: " + teamName;
+     }
+
+     // Define a constructor for dependency injection
+    @Autowired
+     public MyRestController(Coach theCoach) {
+        myCoach = theCoach;
+     }
+
+     @GetMapping("/dailyworkout")
+     public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
      }
 }
