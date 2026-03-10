0afde167889cb27e5d02c40212163a767e09ca32
Aredu89 <aredu_r89@hotmail.com>
2026-03-10 17:47:11 -0500

    Component Scanning

--- src/main/java/com/springboot/demo/MySpringApp/MySpringAppApplication.java
+++ src/main/java/com/springboot/demo/MySpringApp/MySpringAppApplication.java
@@ -3,7 +3,13 @@ package com.springboot.demo.MySpringApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {
+				"com.springboot.demo.MySpringApp.rest",
+				"com.springboot.demo.MySpringApp.entity",
+				"com.springboot.demo.MySpringApp.common"
+		}
+)
 public class MySpringAppApplication {
 
 	public static void main(String[] args) {
