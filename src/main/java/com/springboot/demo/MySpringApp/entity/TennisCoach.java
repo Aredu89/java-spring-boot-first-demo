294c559bf729b517cb481aef040699abb3c7fbee
Aredu89 <aredu_r89@hotmail.com>
2026-03-13 17:07:48 -0500

    Quilifier for contruction injection

---
+++ src/main/java/com/springboot/demo/MySpringApp/entity/TennisCoach.java
@@ -0,0 +1,12 @@
+package com.springboot.demo.MySpringApp.entity;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+}
--- src/main/java/com/springboot/demo/MySpringApp/rest/MyRestController.java
+++ src/main/java/com/springboot/demo/MySpringApp/rest/MyRestController.java
@@ -2,6 +2,7 @@ package com.springboot.demo.MySpringApp.rest;
 
 import com.springboot.demo.MySpringApp.entity.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,17 +20,17 @@ public class MyRestController {
      }
 
      // Define a constructor for dependency injection
-    //@Autowired
-     //public MyRestController(Coach theCoach) {
-      //  myCoach = theCoach;
-     //}
+    @Autowired
+     public MyRestController(@Qualifier("tennisCoach") Coach theCoach) {
+        myCoach = theCoach;
+     }
 
     // setter method for dependency injection
     // This function could have any name
-    @Autowired
-    public void setMyCoach(Coach theCoach) {
-        myCoach = theCoach;
-    }
+//    @Autowired
+//    public void setMyCoach(Coach theCoach) {
+//        myCoach = theCoach;
+//    }
 
      @GetMapping("/dailyworkout")
      public String getDailyWorkout() {
