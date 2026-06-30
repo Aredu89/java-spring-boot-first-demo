080d25417b9cbf8b3040df83a50bf88606970f16
Aredu89 <aredu_r89@hotmail.com>
2026-06-30 17:45:07 -0500

    Create student and save in DB from main app

--- src/main/java/com/springboot/demo/MySpringApp/MySpringAppApplication.java
+++ src/main/java/com/springboot/demo/MySpringApp/MySpringAppApplication.java
@@ -1,5 +1,7 @@
 package com.springboot.demo.MySpringApp;
 
+import com.springboot.demo.MySpringApp.dao.StudentDAO;
+import com.springboot.demo.MySpringApp.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +12,8 @@ import org.springframework.context.annotation.Bean;
 				"com.springboot.demo.MySpringApp.rest",
 				"com.springboot.demo.MySpringApp.entity",
 				"com.springboot.demo.MySpringApp.common",
-				"com.springboot.demo.MySpringApp.config"
+				"com.springboot.demo.MySpringApp.config",
+				"com.springboot.demo.MySpringApp.dao"
 		}
 )
 public class MySpringAppApplication {
@@ -20,10 +23,23 @@ public class MySpringAppApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner (String[] args) {
+	public CommandLineRunner commandLineRunner (StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello World from CommandLineRunner");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		// Create the student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Paul", "Wall", "pwall@email.com");
+
+		// Save the student object
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		//Display id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 
 }
