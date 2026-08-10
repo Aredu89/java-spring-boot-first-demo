9617f6654aa4a4ee92254e55a69fce09a626dc09
Aredu89 <aredu_r89@hotmail.com>
2026-08-10 15:17:13 -0500

    Create student and use autom increment from data base

--- src/main/java/com/springboot/demo/MySpringApp/MySpringAppApplication.java
+++ src/main/java/com/springboot/demo/MySpringApp/MySpringAppApplication.java
@@ -25,8 +25,23 @@ public class MySpringAppApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner (StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+//			createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		// Create multiple students
+		System.out.println("Creating 3 student objects ...");
+		Student tempStudent1 = new Student("John", "Doe", "jdoe@email.com");
+		Student tempStudent2 = new Student("Mary", "Public", "mpublic@email.com");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "bapple@email.com");
+
+		// Save the students
+		System.out.println("Saving the students ...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
