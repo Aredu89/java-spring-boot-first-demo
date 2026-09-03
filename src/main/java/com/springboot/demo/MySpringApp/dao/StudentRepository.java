87ceb5f28dc7b6282435068be61962566e554bc1
Aredu89 <aredu_r89@hotmail.com>
2026-09-03 11:08:45 -0500

    Spring Data REST config and pagination

--- src/main/java/com/springboot/demo/MySpringApp/dao/StudentRepository.java
+++ src/main/java/com/springboot/demo/MySpringApp/dao/StudentRepository.java
@@ -2,9 +2,11 @@ package com.springboot.demo.MySpringApp.dao;
 
 import com.springboot.demo.MySpringApp.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(path="students")
 public interface StudentRepository extends JpaRepository<Student, Integer> {
     // No need to write any code.
     // JPA Repository provides all the CRUD operations and more.
